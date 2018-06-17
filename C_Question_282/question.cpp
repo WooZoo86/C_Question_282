ffe3d2c91a9ac6f44c48f2cd75c485b54b9c5326
WooZoo <271005887@qq.com>
2018-06-17 13:25:55 +0800

    add question answears

---
+++ C_Question_282/question.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+/*	first c program	*/
+void Question_001()
+{
+	printf("Hello World!");
+	printf("\n");
+}
+
+/*	a complete c program
+	calculate 10+20 and print the result.
+*/
+void Question_002()
+{
+	int a = 20;
+	int b = 10;
+
+	printf("%d + %d = %d\n", a, b, a + b);
+}
